d35414bbb64a97be86502e3d85b699aa971a7674
alan.blair <KMhVDwtt6TM30bc0zBFV>
2019-11-30 11:31:56 +1000

    Lenovo Add Config to Application Init

--- src/WPEasyLibrary/Interfaces/IWordPressModule.php
+++ src/WPEasyLibrary/Interfaces/IWordPressModule.php
@@ -6,6 +6,8 @@ namespace WPEasyLibrary\Interfaces;
 
 interface IWordPressModule
 {
+    static function getDashboardView();
+    static function getDescription();
     static function activate();
     static function deactivate();
     static function upgrade();
